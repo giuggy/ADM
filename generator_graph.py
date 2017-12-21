d3e8ac9b11668b0832b3a4fb1d5a57eae9e88032
Giulia Rinaldi <giuggy.rinaldi@gmail.com>
2017-12-21 13:17:31 +0100

    Update generator_graph.py

--- generator_graph.py
+++ generator_graph.py
@@ -69,18 +69,12 @@ def generate_graph(path):
         id_conf, id_conf_int = zip(*dic_conf[u]['id_conference'])
         G.node[u]["id_conference_int"], G.node[u]["id_conference"] = list(id_conf), list(id_conf_int)
         G.node[u]["id_publication_int"] = dic_pub[u]
-
-        G.node[v]['author'] = dic_conf[v]['author']
-        id_conf, id_conf_int = zip(*dic_conf[v]['id_conference'])
-        G.node[v]["id_conference_int"], G.node[v]["id_conference"] = list(id_conf), list(id_conf_int)
-        G.node[v]["id_publication_int"] = dic_pub[v]
-
+       
         a, b = set(dic_pub[u]), set(dic_pub[v])
         d['weight'] = 1 - len(a.intersection(b)) / float(len(a.union(b)))
 
     # Add attributes to isolated nodes
-    sub_g = G.subgraph(lst_no_edge)
-    for u in sub_g.nodes():
+    for u in list_no_edge:
         G.node[u]['author'] = dic_conf[u]['author']
         id_conf, id_conf_int = zip(*dic_conf[u]['id_conference'])
         G.node[u]["id_conference_int"] , G.node[u]["id_conference"] = list(id_conf), list(id_conf_int) 
